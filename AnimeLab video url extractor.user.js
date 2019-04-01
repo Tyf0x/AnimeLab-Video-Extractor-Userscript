264895f666d02d95767d7e19c5c3df64c7362f4f
Geoffrey <tyf0x@github.com>
2019-04-01 21:46:10 +1100

    Add update url

--- AnimeLab video url extractor.user.js
+++ AnimeLab video url extractor.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         AnimeLab video url extractor
-// @namespace    http://tyf0x.net/
-// @version      0.1
+// @namespace    https://github.com/Tyf0x
+// @version      0.2
 // @description  Copy the animelab url to the clipboard
 // @author       Tyf0x
 // @match        https://www.animelab.com/player/*
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js
-// @resource     css https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css
+// @resource     css https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.
+// @updateURL    https://github.com/Tyf0x/AnimeLab-Video-Extractor-Userscript/raw/master/AnimeLab%20video%20url%20extractor.user.js
+// @downloadURL  https://github.com/Tyf0x/AnimeLab-Video-Extractor-Userscript/raw/master/AnimeLab%20video%20url%20extractor.user.js
 // @grant        GM_setClipboard
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
